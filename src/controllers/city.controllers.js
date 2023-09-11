0f0e81a06a70d3851ec651397ef943aaf9887cbf
422UR4H <tiagosindra@id.uff.br>
2023-09-11 00:18:46 -0300

    :bug: fix/emptyBody

--- src/controllers/city.controllers.js
+++ src/controllers/city.controllers.js
@@ -1,8 +1,10 @@
 import httpStatus from "http-status";
 import { cityService } from "../services/city.services.js";
+import { errors } from "../errors/errors.js";
 
 async function create(req, res) {
     const { name } = req.body;
+    if (!name) throw errors.unprocessableEntity("empty name for city");
     await cityService.create(name);
     res.sendStatus(httpStatus.CREATED);
 }
--- src/controllers/passenger.controllers.js
+++ src/controllers/passenger.controllers.js
@@ -1,8 +1,11 @@
 import httpStatus from "http-status";
 import { passengerService } from "../services/passenger.services.js";
+import { errors } from "../errors/errors.js";
 
 async function create(req, res) {
     const { firstName, lastName } = req.body;
+    if (!firstName) throw errors.unprocessableEntity("first name empty");
+    if (!lastName) throw errors.unprocessableEntity("last name empty");
     await passengerService.create(firstName, lastName);
     res.sendStatus(httpStatus.CREATED);
 }
